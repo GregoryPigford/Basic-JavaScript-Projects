ec14e1582638eb237eb2e93a83ebfad4623e61ce
GregoryPigford <46973587+GregoryPigford@users.noreply.github.com>
2020-04-21 11:27:31 -0700

    Add files via upload

---
+++ step58.js
@@ -0,0 +1,5 @@
+var city = "Denver", team = " Broncos";
+var city = city.fontcolor("orange");
+var team = team.fontcolor("blue");
+window.alert("What team won Super Bowl 50?");
+document.write(city + team);
